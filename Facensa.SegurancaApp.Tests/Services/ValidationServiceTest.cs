614e994c5877079699d33f857642dfaf30550847
mmittmann <mmittmann@outlook.com>
2014-08-05 00:50:11 -0700

    Estrutura do projeto

---
+++ Facensa.SegurancaApp.Tests/Services/ValidationServiceTest.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace Facensa.SegurancaApp.Tests.Services
+{
+    [TestClass]
+    public class ValidationServiceTest
+    {
+        [TestMethod]
+        public void Has()
+        {
+        }
+    }
+}
